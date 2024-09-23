b85fbb45c03ccb20356fd2ddcfaf8ff75ca9678b
Cristian Torres Arboleda <cristorres9917@outlook.com>
2024-09-23 15:43:19 -0500

    Se añade el modulo completo de productos

--- routes/productos.js
+++ routes/productos.js
@@ -1,17 +1,10 @@
 const { Router } = require('express');
 const { crearProducto, obtenerProductos, obtenerProductoPorId, actualizarProducto, eliminarProducto } = require('../controllers/productos');
-const { check } = require('express-validator');
-const validarCampos = require('../middlewares/validarCampos');
 
 const router = Router();
 
 // Crear un nuevo producto
-router.post('/', [
-    check('nombreProducto', 'El nombre del producto es obligatorio').not().isEmpty(),
-    check('precio', 'El precio es obligatorio y debe ser un número').isNumeric(),
-    check('categoria', 'La categoría es obligatoria').not().isEmpty(),
-    validarCampos
-], crearProducto);
+router.post('/', crearProducto);
 
 // Obtener todos los productos
 router.get('/', obtenerProductos);
@@ -20,12 +13,7 @@ router.get('/', obtenerProductos);
 router.get('/:id', obtenerProductoPorId);
 
 // Actualizar un producto
-router.put('/:id', [
-    check('nombreProducto', 'El nombre del producto es obligatorio').not().isEmpty(),
-    check('precio', 'El precio es obligatorio y debe ser un número').isNumeric(),
-    check('categoria', 'La categoría es obligatoria').not().isEmpty(),
-    validarCampos
-], actualizarProducto);
+router.put('/:id', actualizarProducto);
 
 // Eliminar un producto
 router.delete('/:id', eliminarProducto);
